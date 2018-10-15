cec74898737c9a19d54fd199511e08aaafb6e3ca
Eduardo Hass <eduardohass@outlook.com>
2018-10-15 13:32:08 -0300

    refatoração relacionada as configurações de comunicação com o banco de dados

--- app/routes/ministerios.js
+++ app/routes/ministerios.js
@@ -1,12 +1,9 @@
+var dbConnection = require('../infra/dbConnection');
+
 module.exports = function (app) {
     app.get('/ministerios', function (req, res) {
         var mysql = require('mysql');
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'church_db'
-        });
+        var connection = dbConnection();
 
         connection.query('select * from ministerio', function (err, results) {
             res.render('ministerios/lista', {lista: results});
