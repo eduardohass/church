fd2ccde702430353255a52f72909d99081892fdc
Eduardo Hass <eduardohass@outlook.com>
2018-10-15 13:26:30 -0300

    inclusão das funcionalidades de comunicação com o banco de dados mysql

--- app/routes/ministerios.js
+++ app/routes/ministerios.js
@@ -1,5 +1,17 @@
 module.exports = function (app) {
     app.get('/ministerios', function (req, res) {
-        res.render('ministerios/lista');
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'church_db'
+        });
+
+        connection.query('select * from ministerio', function (err, results) {
+            res.render('ministerios/lista', {lista: results});
+        });
+
+        connection.end();
     });
 }
