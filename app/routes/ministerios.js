f90d629c5b60be8815c4b7dbdfd722a4426b0e15
Eduardo Hass <eduardohass@outlook.com>
2018-10-16 14:02:14 -0300

    Isolando o acesso aos dados do banco

--- app/routes/ministerios.js
+++ app/routes/ministerios.js
@@ -1,8 +1,9 @@
 module.exports = function (app) {
     app.get('/ministerios', function (req, res) {
         var connection = app.infra.connectionFactory();
+        var ministeriosBanco = app.infra.ministeriosBanco;
 
-        connection.query('select * from ministerio', function (err, results) {
+        ministeriosBanco.lista(connection, function (err, results) {
             res.render('ministerios/lista', { lista: results });
         });
 
--- config/express.js
+++ config/express.js
@@ -1,10 +1,10 @@
-var express = require('express')();
+var express = require('express');
 var load = require('express-load');
 
 module.exports = function () {
     var app = express();
-    express.set('view engine', 'ejs');
-    express.set('views', './app/views');
+    app.set('view engine', 'ejs');
+    app.set('views', './app/views');
     load('routes', { cwd: 'app' })
         .then('infra')
         .into(app);
